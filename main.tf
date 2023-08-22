2c5c6d2c8f51dba3fc1a223e0120c99c4da44fc5
truebala <131427463+truebala@users.noreply.github.com>
2023-08-22 13:35:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,8 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1a"  
 }
-resource "aws_s3_bucket" "rajubala1234" {
-  bucket = "testing-s3-with-terraform2000"
-  acl = "private"
-  versioning {
-       enabled = "true"
-  }
-  tags = {
-    Name = "bucket2000"
-    Environment = "Test"
-  }
-~                                      
+
+resource "aws_s3_bucket" "bala123" {
+  bucket = "bala123" 
+  acl    = "private"
+}
